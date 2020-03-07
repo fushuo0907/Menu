d269efacee2840a01b81503dc19e3bab213cf97d
fushuo0907 <tcswfs@sina.com>
2020-03-07 22:43:31 +0800

    rename menu

--- menu.py
+++ menu.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
     path = "./images"
     image_type = os.listdir(path)
     menu_file = open("./ReadMe.md", "w+")#, encoding='utf-8')
+    menu_file_local = open("./menu.md", "w+")#, encoding='utf-8')
     for i in image_type:
         # print("# {}".format(i))
         menu_file.write("# {}\n".format(i))
+        menu_file_local.write("# {}\n".format(i))
         dir_name = os.path.join(path, i)
         image_list = [os.path.join(dir_name, pic) for pic in os.listdir(dir_name)]
         for image_path in image_list:
@@ -20,7 +22,9 @@ if __name__ == '__main__':
             print(food_name)
             new_image_path = "https://github.com/fushuo0907/Menu/blob/master/images/{}/{}.jpeg".format(i, food_name)
             menu_file.write("#### {}\n".format(food_name))
+            menu_file_local.write("#### {}\n".format(food_name))
             menu_file.write("![{}]({})\n".format(food_name, new_image_path))
+            menu_file_local.write("![{}]({})\n".format(food_name, image_path))
 
 
 
